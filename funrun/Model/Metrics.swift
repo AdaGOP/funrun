7b639d84491010d1108aed136761143a9cdda428
David <david_gunawan_@hotmail.com>
2024-05-15 10:36:41 +0700

    Move Metrics Struct

--- funrun/Model/Metrics.swift
+++ funrun/Model/Metrics.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct DistanceMetric {
+    var name: String
+    var value: Double
+    var unit: String
+}
+
+struct MovementMetric {
+    var movement: String
+    var numOfSet: Int
+    var unit: String
+}
+
+extension DistanceMetric: CustomStringConvertible {
+    var description: String {
+        let formattedValue = String(format: "%.2f", value)
+        return "\(name): \(formattedValue) \(unit)"
+    }
+}
+
+extension MovementMetric: CustomStringConvertible {
+    var description: String {
+        return "\(movement): \(numOfSet) \(unit)"
+    }
+}
